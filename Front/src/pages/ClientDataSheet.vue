9a23d17141078c0be15388a3f35586f3af761bb3
luis ibañez <libanez@i-med.cl>
2021-07-11 03:47:03 -0400

    se actualiza paleta de colores de ficha técnica

--- Front/src/pages/ClientDataSheet.vue
+++ Front/src/pages/ClientDataSheet.vue
@@ -3,7 +3,7 @@
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-size-66">
         <md-card>
-          <md-card-header>
+          <md-card-header data-background-color="pruebacolor">
             <h4 class="title">Ficha Técnica Paciente</h4>
             <p class="category">
               Ingrese los datos necesarios para su Ficha Técnica
@@ -64,7 +64,7 @@
                 </div>
               </div>
               <div class="md-layout-item md-medium-size-100 md-size-50">
-                <md-button class="md-primary md-block" v-on:click="submit"
+                <md-button data-background-color="colorboton" class="md-primary md-block" v-on:click="submit"
                   >Enviar</md-button
                 >
               </div>
